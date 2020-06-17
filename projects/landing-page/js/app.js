7c9bf9ece62a73f4c184efd1edbe050709889b2f
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-06-17 22:00:49 +0200

    feat: Populate nav automatically

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,25 +1,28 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
+
+const navBar = document.getElementById("navbar__list");
+
 let scrollTimer = null;
 
-let activeSection = document.querySelector('section');
+let activeSection = document.querySelector("section");
 console.log(activeSection);
 console.log(getVisibleLandingContainer());
 
@@ -27,23 +30,26 @@ console.log(getVisibleLandingContainer());
  * End Global Variables
  *
  * Start Helper Functions
- * 
-*/
+ *
+ */
 function isVisible(section) {
   const bounds = section.getBoundingClientRect();
   return bounds.top >= 0 || bounds.bottom >= 0;
 }
 
 function getVisibleLandingContainer() {
-  const landingContainers = [...document.querySelectorAll('div.landing__container')];
-  return landingContainers.find(isVisible);
+  return getAllLandingContainers().find(isVisible);
+}
+
+function getAllLandingContainers() {
+  return [...document.querySelectorAll("div.landing__container")];
 }
 
 function activateSection() {
   const sectionToActivate = getVisibleLandingContainer().parentElement;
   if (sectionToActivate !== activeSection) {
     // TODO: Optimize redraw/reflow
-    [activeSection, sectionToActivate].forEach(s => s.classList.toggle('active'));
+    [activeSection, sectionToActivate].forEach((s) => s.classList.toggle("active"));
     activeSection = sectionToActivate;
   }
   scrollTimer = null;
@@ -57,14 +63,26 @@ function handleScroll(event) {
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
+function buildNavBar() {
+  const fragment = document.createDocumentFragment();
+  const navigableSections = getAllLandingContainers().map((l) => l.parentElement);
+  navigableSections.map((section) => buildNavItem(section)).forEach((li) => fragment.appendChild(li));
+  navBar.appendChild(fragment);
+}
 
+function buildNavItem(section) {
+  const li = document.createElement("LI");
+  const liText = section.getAttribute("data-nav");
+  const liTarget = section.id;
+  li.innerHTML = `<span data-target="${liTarget}" class="menu__link">${liText}</span>`;
+  return li;
+}
 
 // Add class 'active' to section when near top of viewport
-
 
 // Scroll to anchor ID using scrollTO event
 
@@ -72,13 +90,13 @@ function handleScroll(event) {
  * End Main Functions
  * Begin Events
  *
-*/
+ */
 
-// Build menu 
-
+// Build menu
+buildNavBar();
 // Scroll to section on link click
 
 // Set sections as active
 
-window.addEventListener('scroll', (event) => handleScroll(event));
-console.log('Scipt loaded!');
+window.addEventListener("scroll", (event) => handleScroll(event));
+console.log("Scipt loaded!");
