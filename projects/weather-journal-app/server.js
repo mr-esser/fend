5ca3a552c4f2167fb13bf298970c7960c5e03089
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-07-26 20:57:02 +0200

    feat: Setup server
    Creating an express app that uses body-parser and cors.
    Setting up a simple callback message announcing port on launch.
    Configured a dummy server route.

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,28 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Express for routing
+const express = require('express');
+const app = express();
 
-// Start up an instance of app
-
-/* Middleware*/
-// Here we are configuring express to use body-parser as middle-ware.
+// Body-parser for message parsing
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+app.get('/hello', function(req, res) {
+  res.send('Hello user :-)');
+});
+
+const port = 3030;
+app.listen(port, function() {
+  console.log(`Weather journal app is listening on port ${port}!`);
+});
