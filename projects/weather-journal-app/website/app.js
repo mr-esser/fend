8b19b52ce7492e282bf9accb8aad3644ce805d8e
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-08-08 21:07:58 +0200

    refactor: Re-arrange main handler

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -8,13 +8,12 @@ const UNITS = 'metric';
 // Cache to help avoid request rejections due to excessive request rate
 const weatherDataCache = new Map();
 
-
-/* Helper functions */
+/* **** Begin general helper functions **** */
 // Today's date printed in an unambiguous format (e.g.: 'Thu Jan 01 1970').
 // App could be up for a long time, so not making this a constant.
-function createDate() {
+const createDisplayDate = function() {
   return new Date().toDateString();
-}
+};
 
 const getWeatherServiceUrl = function(zipAndCountryCode) {
   return `http://api.openweathermap.org/data/2.5/weather?zip=${zipAndCountryCode}&units=${UNITS}&appid=${WEATHER_API_KEY}`;
@@ -24,46 +23,67 @@ const getJournalServiceUrl = function(route = 'all') {
   return `http://localhost:3030/${route}`;
 };
 
-const getWeatherDataFromCache = function(zipAndCountryCode) {
-  let result;
-  if (weatherDataCache.has(zipAndCountryCode)) {
-    const cachedResponse = weatherDataCache.get(zipAndCountryCode);
-    const lastUpdated = cachedResponse.timestamp;
-    const elapsedTime = Date.now() - lastUpdated;
-    if (elapsedTime <= REQUEST_PAUSE) {
-      result = cachedResponse.response;
-    } else {
-      weatherDataCache.delete(zipAndCountryCode);
-    }
+/* Will not validate the input! */
+const getInputText = function(selector) {
+  const element = document.querySelector(selector);
+  if (element && 'value' in element) {
+    return element.value.trim();
   }
-  return result;
+  return '';
 };
 
-const cacheWeatherData = function(zipAndCountryCode, weatherData) {
-  weatherDataCache.set(zipAndCountryCode, {
-    timestamp: Date.now(),
-    response: weatherData,
-  });
+/* Update the most recent entry in the UI */
+const updateUI = function(date='', location='', temperature='', content='') {
+  const container = document.querySelector('#entryHolder');
+  // TODO: Avoid several reflows. Check if really faster this way.
+  container.style = 'display: none;';
+  container.querySelector('#date').innerHTML = date;
+  container.querySelector('#location').innerHTML = location;
+  container.querySelector('#temp').innerHTML = temperature;
+  container.querySelector('#content').innerHTML = content;
+  container.style = '';
 };
+/* **** End general helper functions **** */
 
-const fetchWeatherDataFromService = async function(zipAndCountryCode, units) {
-  const serviceUrl = getWeatherServiceUrl(zipAndCountryCode, units);
-  // Note(!): fetch will only reject on network errors!
-  const response = await fetch(serviceUrl);
-  return response.json();
-};
+/* Function to GET weather data from third-party service.
+ * Will attempt to cache requests. */
+const getWeatherData = async function(zipAndCountryCode) {
+  const cacheWeatherData = function(zipAndCountryCode, weatherData) {
+    weatherDataCache.set(zipAndCountryCode, {
+      timestamp: Date.now(),
+      response: weatherData,
+    });
+  };
+  const fetchWeatherDataFromService = async function(zipAndCountryCode) {
+    const serviceUrl = getWeatherServiceUrl(zipAndCountryCode);
+    // Note(!): fetch will only reject on network errors!
+    const response = await fetch(serviceUrl);
+    return response.json();
+  };
+  const getWeatherDataFromCache = function(zipAndCountryCode) {
+    let result;
+    if (weatherDataCache.has(zipAndCountryCode)) {
+      const cachedResponse = weatherDataCache.get(zipAndCountryCode);
+      const lastUpdated = cachedResponse.timestamp;
+      const elapsedTime = Date.now() - lastUpdated;
+      if (elapsedTime <= REQUEST_PAUSE) {
+        result = cachedResponse.response;
+      } else {
+        weatherDataCache.delete(zipAndCountryCode);
+      }
+    }
+    return result;
+  };
 
-/* Function to GET data from third-party service */
-const getWeatherData = async function(zipAndCountryCode, units) {
   let data = getWeatherDataFromCache(zipAndCountryCode);
   if (!data) {
-    data = await fetchWeatherDataFromService(zipAndCountryCode, units);
+    data = await fetchWeatherDataFromService(zipAndCountryCode);
     cacheWeatherData(zipAndCountryCode, data);
   }
   return data;
 };
 
-/* Function to POST project data */
+/* Function to POST project data to sever */
 const postJournalData = async function(url = '', journalData = {}) {
   // Note(!): fetch will only reject on network errors!
   const response = await fetch(url, {
@@ -76,88 +96,58 @@ const postJournalData = async function(url = '', journalData = {}) {
   return response.json();
 };
 
-/* Function to GET project data */
+/* Function to GET project data from server */
 const getJournalData = async function(url = '') {
   // Note(!): fetch will only reject on network errors!
   const response = await fetch(url);
   return response.json();
 };
 
-/* Named function called by event listener */
+/* MAIN function called by event listener on 'Generate' button */
 const handleGenerate = async function handleGenerate(event) {
-  // Read zip code. Accepting anything here.
-  const zipInput = document.querySelector('#zip');
-  const zipCode = zipInput.value.trim();
-
-  // Read feelings. Accepting anything here.
-  const feelingsTextArea = document.querySelector('#feelings');
-  const feelingsText = feelingsTextArea.value.trim();
-
-  // GET weather data
-  const weatherData = await getWeatherData(zipCode);
-  console.log('Weather service response is: ' + weatherData.cod);
-  // TODO: Throw exception if weather data is not ok: cod !== 200
-
-  // Extract temperature. Unit is hard-coded. See constant UNITS.
-  // Try-catch just in case that retrieved data is incomplete.
-  let temperature;
-  try {
-    temperature = `${weatherData.main.temp} °C`;
-  } catch (e) {
-    temperature = 'unavailable';
-  }
-  console.log('Temp: ' + temperature);
-
-  // Extract location.
-  // Try-catch just in case that retrieved data is incomplete.
-  let location;
-  try {
-    location = `${weatherData.name}, ${weatherData.sys.country}`;
-  } catch (e) {
-    location = 'unavailable';
-  }
-  console.log('Location: ' + location);
-
-  // TODO: Build accumulated data when network errors occurred.
-  // Build journal data (date, location, temperature, feelings)
-  const accumulatedData = {
-    date: createDate(),
-    temp: temperature,
-    location: location,
-    content: feelingsText,
+  const buildJournalData = async function() {
+    const journalData = {
+      date: createDisplayDate(),
+      location: 'unavailable',
+      temperature: 'unavailable',
+      content: getInputText('#feelings'),
+    };
+    try {
+      const weatherData = await getWeatherData(getInputText('#zip'));
+      console.log('Weather service response is: ' + weatherData.cod);
+      // Temperature unit is hard-coded. See constant UNITS.
+      journalData.temperature = `${weatherData.main.temp} °C`;
+      journalData.location = `${weatherData.name}, ${weatherData.sys.country}`;
+    } catch (error) {
+      // Data is incomplete, but it can still be posted and displayed.
+      // So log and then go on.
+      console.log(error);
+    }
+    return journalData;
   };
 
-  // then POST to Journal App
-  await postJournalData(getJournalServiceUrl(), accumulatedData);
-  // TODO: May throw error when server is down. Propagate.
+  const synchronizeWithServer = async function(journalData) {
+    await postJournalData(getJournalServiceUrl(), journalData);
+    return await getJournalData(getJournalServiceUrl());
+  };
 
-  // then GET From Journal App
-  const journalData = await getJournalData(getJournalServiceUrl());
-  // TODO: May throw error when server is down. Propagate.
-
-  // then update the UI (hide outer container + set innerHtml)
-  const container = document.querySelector('#entryHolder');
-  // TODO: Avoid several reflows. Check if really faster this way.
-  container.style = 'display: none;';
-  const divDate = container.querySelector('#date');
-  divDate.innerHTML = journalData.date;
-  const divLocation = container.querySelector('#location');
-  divLocation.innerHTML = journalData.location;
-  const divTemp = container.querySelector('#temp');
-  divTemp.innerHTML = journalData.temp;
-  const divContent = container.querySelector('#content');
-  divContent.innerHTML = journalData.content;
-  container.style = '';
-
-  // TODO: Mighty error catch block that will update most recent entry on
+  try {
+    const journalData = buildJournalData();
+    const serverData = synchronizeWithServer(journalData);
+    updateUI(serverData.date, serverData.location, serverData.temperature,
+        serverData.content);
   // unrecoverable networking errors; i.e.: fetch rejected and threw.
+  } catch (serverError) {
+    updateUI('Something went terribly wrong. Please, try again later.'
+        , JSON.stringify(serverError)
+    );
+  }
 };
 
-// Event listener to add function to existing HTML DOM element
+// Event listener to add function to existing 'Generate' button.
 window.addEventListener('DOMContentLoaded', () => {
   const generateButton = document.querySelector('#generate');
-  // Note: Button does not generate a form submit event here
-  // because there is no such form in the HTML.
+  // Note(!) Button does not generate 'submit' event here
+  // because there is no form involved.
   generateButton.addEventListener('click', handleGenerate);
 });
-
