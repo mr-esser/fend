f8959b0fd50d1c9725d1991e0b124c9c3f616db0
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-08-28 23:49:21 +0200

    refactor: Use npm package for tooltips
    Replaces the custom hand-crafted css.

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -4,11 +4,14 @@ import {handleSubmit} from './js/formHandler';
 import {checkForName, onBlur} from './js/nameChecker';
 
 // Note(!): Sequence is essential here. 'resets' must come first!
+// And then the css-tooltips so that they can be overridden later.
 import './styles/resets.scss';
+import 'modules/css-tooltip/dist/css-tooltip.css';
 import './styles/base.scss';
 import './styles/header.scss';
 import './styles/form.scss';
 import './styles/main.scss';
 import './styles/footer.scss';
 import 'modules/spinkit/spinkit.css';
+
 export {handleSubmit, checkForName, onBlur};
