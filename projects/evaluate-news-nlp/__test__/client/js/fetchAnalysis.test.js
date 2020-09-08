48248aaebb6d6ea28aff05b6df7f89c8acfc3ec5
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-09-08 20:44:51 +0200

    chore: Add tests for new fetch module

---
+++ projects/evaluate-news-nlp/__test__/client/js/fetchAnalysis.test.js
@@ -0,0 +1,5 @@
+import {fetchAnalysisResult} from './../../../src/client/js/fetchAnalysis';
+
+test('All API functions should be accessible', () => {
+  expect(fetchAnalysisResult).toBeDefined();
+});
