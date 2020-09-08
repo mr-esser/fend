3cd0aa7d5343f1258344e900f59da9783139d187
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-09-08 20:45:05 +0200

    chore: Extend tests for handler

--- projects/evaluate-news-nlp/__test__/client/js/formHandler.test.js
+++ projects/evaluate-news-nlp/__test__/client/js/formHandler.test.js
@@ -1,5 +1,11 @@
-import {handleSubmit} from '../../../src/client/js/formHandler';
+import {handleSubmit} from './../../../src/client/js/formHandler';
 
 test('handleSubmit should be defined', () => {
   expect(handleSubmit).toBeDefined();
+});
+
+test('handleSubmit should be read-only', () => {
+  expect(() => {
+    handleSubmit = 'foo';
+  }).toThrow('"handleSubmit" is read-only.');
 });
